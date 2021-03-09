6887a15e1aa2df33c604ce6bfc4c7b198dffc6a2
sergmister <sergmister1@gmail.com>
2021-03-08 22:31:21 -0700

    trigger rebuild

--- src/main.ts
+++ src/main.ts
@@ -193,7 +193,7 @@ const config: Phaser.Types.Core.GameConfig = {
     default: "arcade",
     arcade: {
       gravity: { y: 300 },
-      debug: true,
+      debug: false,
     },
   },
   scene: [MyScene],
